4d297a7dd4e97642f9c7724fca9d4173b193aa47
huangzhe <603850549@qq.com>
2022-06-03 17:57:57 +0800

    新增代码

--- src/apis/menu/menu.controller.ts
+++ src/apis/menu/menu.controller.ts
@@ -26,13 +26,7 @@ export class MenuController {
   }
 
   @Get()
-  getList(
-    // @Query('current', new DefaultValuePipe(1), ParseIntPipe) current: number,
-    // @Query('pageSize', new DefaultValuePipe(10), ParseIntPipe) pageSize: number,
-    @Query() queryMenuDto: QueryMenuDto,
-    @Request() requset: any,
-  ) {
-    console.log(requset.user);
+  getList(@Query() queryMenuDto: QueryMenuDto, @Request() requset: any) {
     return this.menuService.getList(queryMenuDto);
   }
 
